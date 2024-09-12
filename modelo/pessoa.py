38ca2c5de4782d42740f1cce7280d8beb5f44bd7
Vini <134401761+vini-destruidor19@users.noreply.github.com>
2024-09-12 12:09:44 -0300

    Add files via upload

--- modelo/pessoa.py
+++ modelo/pessoa.py
@@ -1,16 +1,21 @@
 class Para_onde:
-    nome_passageira = ""
-    localização = ""
-    destino = ""
-    ativo = False
+    def __init__(self,nome_passageira, localização_atual, destino, ativo):
+        self.nome_passageira = nome_passageira
+        self.localização_atual = localização_atual
+        self.destino = destino
+        self.ativo = False
+    def __str__(self):
+        return f"{self.nome_passageira} | {self.localização_atual} | {self.destino} | {self.ativo}"
 
-pessoa_alisson = Para_onde()
-pessoa_kauane = Para_onde()
+pessoa_alisson = Para_onde("alisson", "centro","morumbi")
+pessoa_kauane = Para_onde("kauane", "morumbi","vila c")
 
-pessoa_kauane.nome_passageira = "kauane"
-pessoa_kauane.localização= "morumbi"
-pessoa_kauane.destino = "centro"
+
 Para_onde = [pessoa_alisson, pessoa_kauane]
+print(pessoa_alisson)
+print(pessoa_kauane)
 
-print(Para_onde)
+
+
+
 
