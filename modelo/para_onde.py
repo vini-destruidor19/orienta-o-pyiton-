9466cbfc228227002953b6132d27c1f3d29b5443
Vini <134401761+vini-destruidor19@users.noreply.github.com>
2024-09-13 10:42:59 -0300

    Add files via upload

--- modelo/para_onde.py
+++ modelo/para_onde.py
@@ -4,15 +4,24 @@ class Para_onde:
         self.nome_passageira = nome_passageira
         self.localização_atual = localização_atual
         self.destino = destino
-        self.ativo = False
-        destino.viagens.append(self)
+        self._ativo = False
+        Para_onde.viagens.append(self)
 
     def __str__(self):
         return f"{self.nome_passageira} | {self.localização_atual} | {self.destino} | {self.ativo}"
 
     def listar_destino():
-        for destino in destino.viagens:
-           print(f"{destino.nome_passageira} | {destino.localização_atual} | {destino.destino} ")
+        print("""𝑢𝑏𝑒𝑟\n""")
+        print("__________________________________________________________________________________________")
+        print(f"{"nome do passageiro".ljust(24)}| {"localização atual".ljust(20)} | {"destino".ljust(20)} | {"atatus"}")
+        print("__________________________________________________________________________________________")
+
+        for destino in Para_onde.viagens:
+           print(f"{destino.nome_passageira.ljust(20)} | {destino.localização_atual.ljust(20)} | {destino.destino.ljust(20)} ")
+
+    @property
+    def ativo(self):
+        return "⍨ Sem transporte disponivel" if self._ativo else "Ü Ativo"
 
 pessoa_alisson = Para_onde("alisson", "centro","morumbi")
 pessoa_kauane = Para_onde("kauane", "morumbi","vila c")
